c3f34588886088b745d87c4bd44528a2e085728b
Stas <46508483+Stas9iMba@users.noreply.github.com>
2021-04-09 21:27:14 +0700

    slider_and_footer

--- js/main.js
+++ js/main.js
@@ -1,64 +1,33 @@
-<<<<<<< HEAD
-// $(function () {
-//   $('.slider').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     arrows: false,
-//     dots: true,
-//     responsive: [
-//       {
-//         breakpoint: 1175,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 2,
-          
-//         },
-     
-//       },
-//       {
-//         breakpoint: 769,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1,
-          
-//         },
-     
-//       },]
-//   });
-
-//   $('.menu__burger').on('click', function () {
-//     $('.menu__list').toggleClass('menu__list--active');
-//   });
-// });
-=======
 $(function () {
-  $('.slider').slick({
+  $('.carousel__inner').slick({
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     arrows: false,
     dots: true,
-    responsive: [
-      {
-        breakpoint: 1175,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+    // responsive: [
+    //   {
+    //     breakpoint: 1175,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+          
+    //     },
+     
+    //   },
+    //   {
+    //     breakpoint: 769,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+          
+    //     },
+     
+    //   },]
   });
 
-  $('.menu__burger').on('click', function () {
-    $('.menu__list').toggleClass('menu__list--active');
-  });
+//   $('.menu__burger').on('click', function () {
+//     $('.menu__list').toggleClass('menu__list--active');
+//   });
 });
->>>>>>> 0519c5257ebf2f7e4fe40c9a06523d333bc37faa
